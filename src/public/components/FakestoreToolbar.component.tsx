1c17a0badca8271db8b22506139ac1efa6410798
NeoRise <neorise456@gmail.com>
2024-12-19 22:40:44 -0500

    feat: add toolbar

---
+++ src/public/components/FakestoreToolbar.component.tsx
@@ -0,0 +1,9 @@
+export function FakestoreToolbar() {
+    return (
+        <div className="border-b px-4 py-2 ">
+            <div className="container ">
+                <span className="text-lg font-bold text-primary"> Fakestore </span>
+            </div>
+        </div>
+    )
+}
